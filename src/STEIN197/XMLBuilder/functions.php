02fba8810c7b5be32c1b3203393edf3896ab8b68
Nail <nil20122013@gmail.com>
2020-11-06 17:44:22 +0500

    Commented an idea

--- src/STEIN197/XMLBuilder/functions.php
+++ src/STEIN197/XMLBuilder/functions.php
@@ -4,3 +4,31 @@
 	function _(...$arguments): Builder {
 		return new Builder(...$arguments);
 	}
+
+	// TODO: Callbacks like this?
+	_()
+	->html()
+	->body(function($body) {
+		$body
+		->header()
+		->main(function($main) {
+			$main
+			->section(function($section) {
+				$section
+				->div(function($div) {
+					$div
+					->ul(function($ul) {
+						while (true) {
+							$ul->li(function($li) {
+								$li->a();
+							});
+						}
+					});
+					if (true) {
+						$div->p();
+					}
+				});
+			});
+		})
+		->footer();
+	});
